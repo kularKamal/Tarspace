e960eeb0af68d0a4a4b704a431c7208f78e5ee7e
Leonardo Baldin <leobaldin.2000@gmail.com>
2023-08-20 12:58:52 +0200

    feat: added breadcrumb navigation

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,19 +1,31 @@
 import { Link, useLocation } from "react-router-dom"
 
-type BreadcrumbsElement = {
+export type BreadcrumbsElement = {
   name: string
   route: string
 }
 
-interface CrumbNames {
+type CrumbNames = {
   [key: string]: string | null
 }
 
 const crumbNames: CrumbNames = {
-  projects: null,
+  deliverables: null,
 }
 
-const Breadcrumbs = (props: { crumbs?: BreadcrumbsElement[]; className?: string }) => {
+const IconHome = () => (
+  <svg
+    className="w-3 h-3 mr-2.5"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 20 20"
+  >
+    <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+  </svg>
+)
+
+export const Breadcrumbs = (props: { crumbs?: BreadcrumbsElement[]; className?: string }) => {
   const location = useLocation()
 
   const crumbs: BreadcrumbsElement[] =
@@ -43,15 +55,7 @@ const Breadcrumbs = (props: { crumbs?: BreadcrumbsElement[]; className?: string 
             to="/"
             className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
           >
-            <svg
-              className="w-3 h-3 mr-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-            </svg>
+            <IconHome />
             Home
           </Link>
         </li>
@@ -76,5 +80,3 @@ const Breadcrumbs = (props: { crumbs?: BreadcrumbsElement[]; className?: string 
     </nav>
   )
 }
-
-export default Breadcrumbs
